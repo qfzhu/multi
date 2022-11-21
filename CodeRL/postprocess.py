310986674e3f6e5f19fddcb5a822d6d88c904735
qfzhu <qfzhu@ir.hit.edu.cn>
2022-11-21 13:47:18 +0800

    tranfer to human eval format

---
+++ CodeRL/postprocess.py
@@ -0,0 +1,20 @@
+import json
+import sys
+from human_eval.data import write_jsonl
+
+
+def to_human_eval_format(path):
+    samples = []
+    for i in range(164):
+        with open(path + str(i) + '.json') as fr:
+            content = json.load(fr)
+            solutions = content[str(i)]['code']
+            for sol in solutions:
+                samples.append({'task_id': 'HumanEval/' + str(i), 'completion': sol})
+
+    write_jsonl("samples.jsonl", samples)
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    to_human_eval_format(path)
